596cdbe89ce019483cb76ea498a541935d3d0886
Wataru Ishikawa <wataruby@gmail.com>
2025-07-02 01:00:30 +0900

    gRPCサーバーにTLS認証を追加し、セキュリティを強化しました

--- internal/presentation/prepare/register.go
+++ internal/presentation/prepare/register.go
@@ -2,10 +2,13 @@ package prepare
 
 import (
 	"context"
+	"crypto/tls"
+	"os"
 	"query-service/internal/presentation/interceptor"
 
 	"github.com/watarui-go-cqrs/pb/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type QueryServer struct {
@@ -19,8 +22,29 @@ func chainUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.Unar
 	return interceptor.LoggingInterceptor(ctx, req, info, newHandler)
 }
 
+func createCreds() credentials.TransportCredentials {
+	certFile := "internal/presentation/prepare/queryservice.pem"
+	keyFile := "internal/presentation/prepare/queryservice-key.pem"
+
+	cert, err := os.ReadFile(certFile)
+	if err != nil {
+		panic(err)
+	}
+	key, err := os.ReadFile(keyFile)
+	if err != nil {
+		panic(err)
+	}
+	certificate, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		panic(err)
+	}
+	creds := credentials.NewServerTLSFromCert(&certificate)
+	return creds
+}
+
 func NewQueryServer(category pb.CategoryQueryServer, product pb.ProductQueryServer) *QueryServer {
 	serverOpts := []grpc.ServerOption{
+		grpc.Creds(createCreds()),
 		grpc.UnaryInterceptor(chainUnaryInterceptor),
 	}
 	server := grpc.NewServer(serverOpts...)
